af642cefe338fbaeab6d480244f3b2afffdf0b3b
Shakeshack <redfreak97@gmail.com>
2020-11-17 18:47:22 +0800

    Force Init

--- app/src/androidTest/java/com/example/belloapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/belloapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.doorbellapp;
+package com.example.belloapp;
 
 import android.content.Context;
 
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.doorbellapp", appContext.getPackageName());
+
+        assertEquals("com.example.belloapp", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/belloapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/belloapp/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.doorbellapp;
+package com.example.belloapp;
 
 import org.junit.Test;
 
